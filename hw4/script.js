ab02156b403def4039260a692387a7b3a3d4cc54
Oleh Hanala <oleg99gamala@gmail.com>
2022-11-24 12:07:29 +0200

    add ;

--- hw4/script.js
+++ hw4/script.js
@@ -13,13 +13,13 @@ console.log(numOrStr)
 
 switch (true) {
     case numOrStr === null:
-        console.log('ви скасували')
+        console.log('ви скасували');
         break;
     case numOrStr.trim() === '':
         console.log('Empty String');
         break;
     case isNaN( +numOrStr ):
-        console.log(' number is Ba_NaN')
+        console.log(' number is Ba_NaN');
         break;
     default:
         console.log('OK!');
