6596246745c68174dc3b5df400d0cee689b81113
Oleh Hanala <oleg99gamala@gmail.com>
2022-12-29 15:51:19 +0200

    hw16

---
+++ hw16/script.js
@@ -0,0 +1,156 @@
+const ITEM_INFO_LOCAL_STORAGE_KEY = 'item';
+
+const itemList = document.getElementById("item-list");
+const itemInfo = document.getElementById("item-info");
+const checkoutForm = document.getElementById("checkout-user-form");
+const errorsContainer = document.getElementById("errors-container");
+const datalist = document.getElementById("citi-list");
+const cities = ['Київ','Харків','Одеса','Дніпро','Донецьк','Запоріжжя','Львів','Кривий Ріг','Миколаїв','Маріуполь','Луганськ','Севастополь','Вінниця','Макіївка','Сімферополь','Херсон','Полтава','Чернігів','Черкаси','Суми','Житомир','Хмельницький','Чернівці','Горлівка','Рівне','Івано-Франківськ','Кременчук','Тернопіль','Луцьк','Біла Церква','Червоний Луч','Бердянськ','Ужгород','Павлоград'];
+
+const localStorageItemData = {
+    get: () => JSON.parse(localStorage.getItem(ITEM_INFO_LOCAL_STORAGE_KEY)),
+    set: (item) => localStorage.setItem(ITEM_INFO_LOCAL_STORAGE_KEY, JSON.stringify(item)),
+    delete: () => localStorage.removeItem(ITEM_INFO_LOCAL_STORAGE_KEY)
+}
+
+const validateForm = {
+    userName(value) {
+        const pattern = /^[a-zA-Z]+ [a-zA-Z]+$/
+
+        if(value.match(pattern)){
+            return result = value
+        }
+    },
+    phoneNumber(value) {
+        const pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+
+        if(value.match(pattern)){
+            return result = value
+        }
+    },
+    userEmail(value) {
+        const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+
+        if(value.match(pattern)){
+            return result = value
+        }
+    }
+}
+
+let userChoice
+
+itemList.addEventListener("click", onItemListClick);
+checkoutForm.addEventListener("submit", onCheckoutFormSubmit);
+
+function onItemListClick(e) {
+    const item = getItemBySelector(e.target);
+
+    if (item) {
+        if (e.target.classList.contains('buy-btn')) {
+            userChoice = getItemName(item);
+            showForm(setCitiesOnDatalist(cities, datalist));
+            localStorageItemData.delete();
+            clearInfoAboutItemDelivery();
+        } 
+    }
+}
+
+function onCheckoutFormSubmit(e) {
+    e.preventDefault();
+    clearErrors();
+    const userData = getUserInfoFromForm(checkoutForm);
+    localStorageItemData.set(userData);
+
+    if(userData.errorFields.length === 0) {
+        hideForm();
+        resetForm();
+        showInfoAboutItemDelivery(userChoice, userData.userInfo);
+    } else {
+        userData.errorFields.forEach((element) => showErrorFromForm(element));
+    }
+}
+
+function getUserInfoFromForm(form) {
+    const data = new FormData (form);
+    const userInfo = {};
+    const errorFields = [];
+
+    for(const[name, value] of data) {
+        if (name === "firstAndLastName" && !validateForm.userName(value)){
+            errorFields.push(name);
+        } else if (name === "phone" && !validateForm.phoneNumber(value)){
+            errorFields.push(name);
+        } else if(name === "email" && !validateForm.userEmail(value)){
+            errorFields.push(name);
+        } else {
+            if(!value.trim()){
+                errorFields.push(name);
+            } else {
+                userInfo[name] = value;
+            }
+        }
+    }
+    return {userInfo, errorFields};
+}
+
+function showErrorFromForm(name) {
+    const error = document.createElement('li');
+    error.style.color = "red";
+    error.innerText = `вам потрібно заповнити поле з назвою ${name}`;
+    errorsContainer.append(error);
+}
+
+function showInfoAboutItemDelivery(item, data) {
+    const textInfo =  document.createElement('p');
+    const ul = document.createElement("ul");
+    
+    textInfo.innerText = `Ви замовили ${item}`;
+    itemInfo.append(textInfo);
+    
+    for (const key in data) {
+        const li = document.createElement("li");
+        li.innerText = `Your ${key} : ${data[key]} `;
+        ul.appendChild(li);
+    }    
+    itemInfo.append(ul);
+}
+
+function clearInfoAboutItemDelivery() {
+    itemInfo.innerHTML = '';
+}
+
+function clearErrors() {
+    errorsContainer.innerHTML = '';
+}
+
+function setCitiesOnDatalist(cities, formDatalist) {
+    cities.forEach(function(item){
+        const option = document.createElement('option');
+        option.value = item;
+        formDatalist.appendChild(option);
+     });
+}
+
+function getItemName(item) {
+    return item.dataset.itemName;
+}
+
+function getItemBySelector(el) {
+    return el.closest('.item-block');
+}
+
+function showForm() {
+    checkoutForm.style.display = "flex";
+    itemList.style.display = "none";
+    itemInfo.style.display = "none";
+}
+
+function hideForm() {
+    checkoutForm.style.display = "none";
+    itemList.style.display = "flex";
+    itemInfo.style.display = "block";
+}
+
+function resetForm() {
+    checkoutForm.reset();
+}
