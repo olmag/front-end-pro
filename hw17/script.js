1be24f665754afeb61a5d2739baec49f5632e791
Oleh Hanala <oleg99gamala@gmail.com>
2023-01-11 19:54:15 +0200

    new chenging hw17

--- hw17/script.js
+++ hw17/script.js
@@ -1,12 +1,17 @@
-function Person(name, gender) {
-    this.name = name;
-    this.gender = gender;
+class Person {
+    constructor(name, gender){
+        this.name = name;
+        this.gender = gender;
+    }
 }
 
-function Flat() {
-    this.lodgers = [];
+class Flat {
 
-    this.addLodger = function(person) {
+    constructor(){
+        this.lodgers = [];
+    }
+
+    addLodger(person) {
         if (person instanceof Person) {
             this.lodgers.push(person)
            } else {
@@ -15,34 +20,37 @@ function Flat() {
     }
 }
 
-function Bilding(maximumNumberOfApartments) {
-    this.flats = [];
-    this.maximumNumberOfApartments = maximumNumberOfApartments;
-
-    this.addflat = function(flat) {
-        if (flat instanceof Flat && this.flats.length < this.maximumNumberOfApartments) {
-            this.flats.push(flat)
-           } else {
-            throw Error(`object is not instance of Flat or added a large number of apartments (maximum ${this.maximumNumberOfApartments})`)
-         }
+class Bilding {
+    constructor(maximumNumberOfFlats){
+        this.flats = [];
+        this.maximumNumberOfFlats = maximumNumberOfFlats;
     }
 
+    addFlat(flat) {
+        if (flat instanceof Flat && this.flats.length < this.maximumNumberOfFlats) {
+            this.flats.push(flat)
+        } else if(!(flat instanceof Flat) && this.flats.length < this.maximumNumberOfFlats) {
+            throw Error('object is not instance of Flat')
+        } else if(flat instanceof Flat && !(this.flats.length < this.maximumNumberOfFlats)) {
+            throw Error(`Added a large number of apartments (maximum ${this.maximumNumberOfFlats})`)
+        }
+    }
 }
 
-const person1 = new Person('oleg', 'men');
-const person2 = new Person('ole', 'men');
-const person3 = new Person('ole', 'men');
-const person4 = new Person('ole', 'men');
-const person5 = new Person('ole', 'men');
-const person6 = new Person('ole', 'men');
-const person7 = new Person('ole', 'men');
-const person8 = new Person('ole', 'men');
-const person9 = new Person('ole', 'men');
-const person10 = new Person('ole', 'men');
-const person11 = new Person('ole', 'men');
-const person12 = new Person('ole', 'men');
-const person13 = new Person('ole', 'men');
-const person14 = new Person('ole', 'men');
+const person1 = new Person('oleg', 'man');
+const person2 = new Person('ole', 'man');
+const person3 = new Person('ole', 'man');
+const person4 = new Person('ole', 'man');
+const person5 = new Person('ole', 'man');
+const person6 = new Person('ole', 'man');
+const person7 = new Person('ole', 'man');
+const person8 = new Person('ole', 'man');
+const person9 = new Person('ole', 'man');
+const person10 = new Person('ole', 'man');
+const person11 = new Person('ole', 'man');
+const person12 = new Person('ole', 'man');
+const person13 = new Person('ole', 'man');
+const person14 = new Person('ole', 'man');
 
 const flat1 = new Flat();
 flat1.addLodger(person1);
@@ -80,18 +88,21 @@ flat2.addLodger(person13);
 const flat11 = new Flat();
 flat2.addLodger(person14);
 
+const flat12 = {};
+
 const bilding1 = new Bilding(11);
-bilding1.addflat(flat1);
-bilding1.addflat(flat2);
-bilding1.addflat(flat3);
-bilding1.addflat(flat4);
-bilding1.addflat(flat5);
-bilding1.addflat(flat6);
-bilding1.addflat(flat7);
-bilding1.addflat(flat8);
-bilding1.addflat(flat9);
-bilding1.addflat(flat10);
-bilding1.addflat(flat11);
+bilding1.addFlat(flat1);
+bilding1.addFlat(flat2);
+bilding1.addFlat(flat3);
+bilding1.addFlat(flat4);
+bilding1.addFlat(flat5);
+bilding1.addFlat(flat6);
+bilding1.addFlat(flat7);
+bilding1.addFlat(flat8);
+bilding1.addFlat(flat9);
+bilding1.addFlat(flat10);
+bilding1.addFlat(flat11);
+
 
 console.log(bilding1)
 
